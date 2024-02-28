effe58f37fc6532fbc0a3294c7b068dce7b72d65
recleun <98621222+recleun@users.noreply.github.com>
2024-02-29 00:19:22 +0200

    Refactor main program

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ fn get_terminal_size() -> TerminalSize {
 }
 
 fn main() {
-    let buffer = Buffer::new(get_terminal_size());
-    println!("Width, Height: {}", buffer.size);
+
+    let mut changed = true;
+    let mut buffer = Buffer::new(get_terminal_size());
+
+    buffer.set_line(0, &String::from("1 - Start"));
+    buffer.set_line(1, &String::from("2 - Help"));
+    buffer.set_line(2, &String::from("3 - Options"));
+    buffer.set_line(3, &String::from("0 - Exit"));
+
+    loop {
+        if changed {
+            changed = false;
+            buffer.update_display();
+        }
+    }
 }
