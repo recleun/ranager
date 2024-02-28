c6122a9a01d59fa0de7e2cf3ecb4c45c7bc281fe
recleun <98621222+recleun@users.noreply.github.com>
2024-02-28 03:38:30 +0200

    Change output to terminal size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fmt;
+use termion::terminal_size;
+
+struct TerminalSize {
+    x: u16,
+    y: u16,
+}
+
+impl fmt::Display for TerminalSize {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+fn get_terminal_size() -> TerminalSize {
+    let (x, y) = terminal_size().expect("Failed to get terminal size");
+    TerminalSize {
+        x,
+        y,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Width, Height: {}", get_terminal_size());
 }
