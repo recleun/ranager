2d073d67fec586db69a949d886d167f6b2b174f0
recleun <98621222+recleun@users.noreply.github.com>
2024-02-28 03:59:09 +0200

    Add lines to buffer with get method

--- src/buffer.rs
+++ src/buffer.rs
@@ -13,12 +13,25 @@ impl fmt::Display for TerminalSize {
 
 pub struct Buffer {
     pub size: TerminalSize,
+    lines: Vec<String>,
 }
 
 impl Buffer {
     pub fn new(size: TerminalSize) -> Self {
-        Buffer {
-            size
+        let mut lines: Vec<String> = Vec::new();
+        for _ in 0..size.y {
+            lines.push(String::new());
         }
+        Buffer {
+            size,
+            lines,
+        }
+    }
+
+    pub fn get_line(&self, index: u16) -> String {
+        if index > self.size.y {
+            panic!("Trying to reach for line out of reach")
+        };
+        self.lines[index as usize].clone()
     }
 }
