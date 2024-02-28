de6206e9a608c4724868914e39a41588dd87fea0
recleun <98621222+recleun@users.noreply.github.com>
2024-02-28 04:07:25 +0200

    Create set line method for buffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::mem;
 
 pub struct TerminalSize {
     pub x: u16,
@@ -33,5 +34,16 @@ impl Buffer {
             panic!("Trying to reach for line out of reach")
         };
         self.lines[index as usize].clone()
+    }
+
+    pub fn set_line(&mut self, index: u16, line: &String) {
+        if index > self.size.y {
+            panic!("Trying to reach for line out of reach");
+        }
+        self.replace(index as usize, line);
+    }
+
+    fn replace(&mut self, index: usize, line: &String) -> String {
+        mem::replace(&mut self.lines[index], line.to_string())
     }
 }
